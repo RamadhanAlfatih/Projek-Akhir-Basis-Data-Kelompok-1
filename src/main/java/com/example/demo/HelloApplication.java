8f7e7890ec2e5f3b61428f2dbc0ca732c7af0c36
RamadhanAlfatih <ramadhanalfatih10@gmail.com>
2022-11-26 16:30:26 +0700

    gui awal

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("signin.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 678, 431);
         stage.setTitle("Database");
         stage.setScene(scene);
         stage.show();
